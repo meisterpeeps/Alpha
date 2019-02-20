cf48ebcaeda0328e8555ffb80ec91331919450f2
Jeffrey Peoples <jpeoples@thriftbooks.com>
2019-02-19 21:40:00 -0800

    port 80

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const webpack = require('webpack')
 const webpackConfig = require('./webpack.config.js')
 const app = express()
-const port = process.env.PORT || 3000;
+const port = 80;
 const environment = process.env.npm_lifecycle_event; 
 
 
